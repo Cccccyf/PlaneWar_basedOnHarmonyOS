660f19b9ed68ee2100a5f2f64f2894b985ac5a01
Cccccyf <1902383836@qq.com>
2022-04-27 14:48:27 +0800

    Revert "default"
    This reverts commit 3affa1ec59ae518a29e1375ee010b6a529941a2b.

--- entry/src/main/java/com/example/hit_the_plane/slice/RankListAbilitySlice.java
+++
@@ -1,7 +0,0 @@
-package com.example.hit_the_plane.slice;
-
-import ohos.aafwk.ability.AbilitySlice;
-
-public class RankListAbilitySlice extends AbilitySlice {
-
-}
--- entry/src/test/java/com/example/hit_the_plane/ExampleTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.hit_the_plane;
-
-import org.junit.Test;
-
-public class ExampleTest {
-    @Test
-    public void onStart() {
-    }
-}
